ec52746272db47bef8f9ef816692639b3a9ab178
Sun-2302 <hei.dera.932@gmail.com>
2024-08-15 18:45:05 +0300

    feat: add primary color to the link of actual page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,10 @@
 // src/components/Navbar.jsx
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = ({ bg, linkColor, logo }) => {
   const [scrolled, setScrolled] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -21,6 +22,10 @@ const Navbar = ({ bg, linkColor, logo }) => {
     };
   }, []);
 
+  const getLinkClass = (path) => {
+    return location.pathname === path ? 'text-primary' : 'hover:text-primary';
+  };
+
   return (
     <nav className={`fixed flex z-20 justify-between w-full px-32 py-4 transition-all duration-300 ${scrolled ? bg : 'bg-transparent'
       }`}
@@ -31,10 +36,10 @@ const Navbar = ({ bg, linkColor, logo }) => {
       <div
         className={`lg:flex lg:items-center lg:space-x-11 font-semibold ${linkColor}`}
       >
-        <Link to="/home#banner" className="hover:text-primary">Home</Link>
-        <Link to="/about-us#banner" className="hover:text-primary">About</Link>
-        <Link to="/courses#banner" className="hover:text-primary">Courses</Link>
-        <Link to="/contact#banner" className="hover:text-primary">Contact</Link>
+        <Link to="/home#banner" className={getLinkClass('/home')}>Home</Link>
+        <Link to="/about-us#banner" className={getLinkClass('/about-us')}>About</Link>
+        <Link to="/courses#banner" className={getLinkClass('/courses')}>Courses</Link>
+        <Link to="/contact#banner" className={getLinkClass('/contact')}>Contact</Link>
       </div>
     </nav>
   );
