c274ec80654fe6c0c22aeaf8713e9b0d6a3a7a87
fanomezana <hei.fanomezana.2@gmail.com>
2024-08-16 16:20:57 +0300

    style(wip): responsive home banner

--- src/components/home/HomeBanner.jsx
+++ src/components/home/HomeBanner.jsx
@@ -1,4 +1,3 @@
-// src/components/HomeBanner.jsx
 import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import { useLocation } from "react-router-dom";
@@ -10,7 +9,7 @@ const HomeBanner = () => {
     if (location.hash) {
       const element = document.getElementById(location.hash.substring(1));
       if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
+        element.scrollIntoView({ behavior: "smooth" });
       }
     }
   }, [location]);
@@ -23,49 +22,44 @@ const HomeBanner = () => {
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundAttachment: "fixed",
-        
       }}
-      className="overflow-hidden lg:h-[900px] h-[500px]"
+      className="overflow-hidden lg:h-screen h-[500px] flex items-center justify-center"
     >
-      <div className="bg-black overflow-hidden bg-opacity-40 lg:min-h-[900px] min-h-[500px]">
-      <div className="container lg:mx-auto lg:pt-64 pt-28">
-        <div className="flex justify-center">
-          <div className="text-center">
-            <motion.p
-              className="font-semibold lg:text-xl text-background"
-              initial={{ opacity: 0, y: -20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.6 }}
-              viewport={{ once: false, amount: 0.1 }}
-            >
-              BEST ONLINE EDUCATION SERVICE IN THE WORLD
-            </motion.p>
-            <motion.h2
-              className="lg:text-5xl text-xl text-background font-semibold mt-6 mb-12"
-              initial={{ opacity: 0, y: -20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.6, delay: 0.2 }}
-              viewport={{ once: false, amount: 0.1 }}
-            >
-              ONE STEP AHEAD THIS SEASON
-            </motion.h2>
-            <motion.div
-              className="flex lg:flex-row justify-center flex-col overflow-hidden lg:ml-0 ml-24 gap-4"
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.6, delay: 0.4 }}
-              viewport={{ once: false, amount: 0.1 }}
-            >
-              <motion.button className="bg-primaryGreen border border-secondaryGreen text-background text-xs font-semibold w-44 px-12 py-4 rounded-lg hover:bg-transparent">
-                LEARN MORE
-              </motion.button>
-              <motion.button className="bg-secondaryBrown border border-secondaryBrown text-background text-xs font-semibold w-44 lg:px-12 px-2 py-4 rounded-lg hover:bg-transparent hover:text-primary">
-                SEE COURSE
-              </motion.button>
-            </motion.div>
-          </div>
+      <div className="bg-black bg-opacity-40 w-full h-full flex flex-col items-center justify-center px-4 lg:px-0">
+        <div className="text-center">
+          <motion.p
+            className="font-semibold lg:text-xl text-background"
+            initial={{ opacity: 0, y: -20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: false, amount: 0.1 }}
+          >
+            BEST ONLINE EDUCATION SERVICE IN THE WORLD
+          </motion.p>
+          <motion.h2
+            className="lg:text-5xl text-xl text-background font-semibold mt-6 mb-12"
+            initial={{ opacity: 0, y: -20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.2 }}
+            viewport={{ once: false, amount: 0.1 }}
+          >
+            ONE STEP AHEAD THIS SEASON
+          </motion.h2>
+          <motion.div
+            className="flex flex-col lg:flex-row gap-4 justify-center"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.4 }}
+            viewport={{ once: false, amount: 0.1 }}
+          >
+            <motion.button className="bg-primaryGreen border border-secondaryGreen text-background text-xs font-semibold w-full max-w-[200px] px-8 py-4 rounded-lg hover:bg-transparent">
+              LEARN MORE
+            </motion.button>
+            <motion.button className="bg-secondaryBrown border border-secondaryBrown text-background text-xs font-semibold w-full max-w-[200px] px-8 py-4 rounded-lg hover:bg-transparent hover:text-primary">
+              SEE COURSE
+            </motion.button>
+          </motion.div>
         </div>
-      </div>
       </div>
     </section>
   );
