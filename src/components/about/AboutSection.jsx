8c007bb86d2b43d91e7cedfc8a77b0b7ffcc514b
fanomezana <hei.fanomezana.2@gmail.com>
2024-08-16 15:57:32 +0300

    style: center about section component

--- src/components/about/AboutSection.jsx
+++ src/components/about/AboutSection.jsx
@@ -7,20 +7,20 @@ import { motion } from 'framer-motion';
 const AboutSection = () => {
   return (
     <section className="py-20">
-      <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row items-center mr-32">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col lg:flex-row items-center gap-8 lg:gap-16">
           <motion.div
-            className="lg:w-1/2"
+            className="lg:w-1/2 -mt-12"
             initial={{ opacity: 0, x: -50 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.6, ease: "easeInOut", delay: 0.4 }}
           >
-            <div className="relative w-[540px] ml-24 mt-14">
-              <img className="img-fluid w-full" src="images/about-us.jpg" alt="About" />
+            <div className="relative w-full max-w-[540px] mx-auto lg:ml-24 mt-14">
+              <img className="img-fluid w-full h-auto " src="images/about-us.jpg" alt="About" />
             </div>
           </motion.div>
           <motion.div
-            className="mt-8 border-[10px] border-background rounded-md py-[75px] px-[80px] w-[700px]"
+            className="border-[10px] border-background rounded-md py-12 px-8 w-full max-w-[700px] mx-auto lg:mx-0 lg:py-[75px] lg:px-[80px] "
             initial={{ opacity: 0, x: 50 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.6, ease: "easeInOut" }}
@@ -33,7 +33,7 @@ const AboutSection = () => {
                   Cattle greater brought sixth fly den dry good tree isn't seed stars were.
                 </p>
                 <p className="mb-8 text-gray-600">
-                  Subdue whales void god which living don't midst lesser yieldi over lights whose.
+                  Subdue whales void god which living don't midst lesser yielding over lights whose.
                   Cattle greater brought sixth fly den dry good tree isn't seed stars were the boring.
                 </p>
                 <a className="inline-flex items-center text-background bg-primaryGreen px-12 py-4 rounded-md hover:bg-secondaryBrown hover:text-background font-semibold text-xs" href="#">
